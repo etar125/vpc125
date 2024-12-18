a05030c8e6523148a613c42a526eb6df24175c91
etar125 <inniesharpsoftware@gmail.com>
2024-12-18 15:29:55 +0300

    Вынесены переменные
    Вынесены переменные opcode, adr, val1, val2 из цикла.

--- src/e125v1/e125v1.h
+++ src/e125v1/e125v1.h
@@ -40,66 +40,68 @@ typedef enum {
 void e125_run(e125_pc *m, byte pos) {
     if(pos >= m->other) {
         m->position = pos;
+        e125_opcodes opcode;
+        byte adr, val1, val2;
         while(m->position < 255) {
-            e125_opcodes opcode = e125_getMem(m, m->position);
+            opcode = e125_getMem(m, m->position);
 
             if(opcode == End) break;
 
             else if(opcode == MoveA) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val = e125_getMem(m, m->position++);
-                e125_loadMem(m, val, adr);
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, m->position++);
+                e125_loadMem(m, val1, adr);
             } else if(opcode == MoveB) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val = e125_getMem(m, e125_getMem(m, m->position++));
-                e125_loadMem(m, val, adr);
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, e125_getMem(m, m->position++));
+                e125_loadMem(m, val1, adr);
             }
 
             else if(opcode == AddA) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, e125_getMem(m, m->position++));
-                byte val2 = e125_getMem(m, e125_getMem(m, m->position++));
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, e125_getMem(m, m->position++));
+                val2 = e125_getMem(m, e125_getMem(m, m->position++));
                 e125_loadMem(m, val1 + val2, adr);
             } else if(opcode == AddB) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, e125_getMem(m, m->position++));
-                byte val2 = e125_getMem(m, m->position++);
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, e125_getMem(m, m->position++));
+                val2 = e125_getMem(m, m->position++);
                 e125_loadMem(m, val1 + val2, adr);
             } else if(opcode == AddC) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, m->position++);
-                byte val2 = e125_getMem(m, m->position++);
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, m->position++);
+                val2 = e125_getMem(m, m->position++);
                 e125_loadMem(m, val1 + val2, adr);
             }
 
             else if(opcode == SubstractA) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, e125_getMem(m, m->position++));
-                byte val2 = e125_getMem(m, e125_getMem(m, m->position++));
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, e125_getMem(m, m->position++));
+                val2 = e125_getMem(m, e125_getMem(m, m->position++));
                 e125_loadMem(m, val1 - val2, adr);
             } else if(opcode == SubstractB) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, e125_getMem(m, m->position++));
-                byte val2 = e125_getMem(m, m->position++);
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, e125_getMem(m, m->position++));
+                val2 = e125_getMem(m, m->position++);
                 e125_loadMem(m, val1 - val2, adr);
             } else if(opcode == SubstractB) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, m->position++);
-                byte val2 = e125_getMem(m, e125_getMem(m, m->position++));
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, m->position++);
+                val2 = e125_getMem(m, e125_getMem(m, m->position++));
                 e125_loadMem(m, val1 - val2, adr);
             } else if(opcode == SubstractD) {
                 m->position++;
-                byte adr = e125_getMem(m, m->position++);
-                byte val1 = e125_getMem(m, m->position++);
-                byte val2 = e125_getMem(m, m->position++);
+                adr = e125_getMem(m, m->position++);
+                val1 = e125_getMem(m, m->position++);
+                val2 = e125_getMem(m, m->position++);
                 e125_loadMem(m, val1 - val2, adr);
             } else m->position++;
         } m->position = 0;
